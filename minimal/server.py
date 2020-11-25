5c2e312637b0b85a74da30d12fbc48ec8ecbbb30
Jarppe <jarppe@metosin.fi>
2020-11-25 20:40:29 +0200

    initial

---
+++ minimal/server.py
@@ -0,0 +1,14 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("Hello\n".encode("utf8"))
+
+
+if __name__ == "__main__":
+    httpd = HTTPServer(("0.0.0.0", 8080), Handler)
+    httpd.serve_forever()
